586f565a4acef6a36f3671811f3f8e9712a13336
Savio Freitas <saviofreitas017@gmail.com>
2024-10-31 14:39:35 -0300

    javascript rodando
    terminei a parte de js e agora falta terminar de arrumar com o style ou seja com css e alterar uma coisinha só no html, aii acabou

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert('Bem vindo a página!')
+function carregar(){
+
+    var msg = window.document.getElementById('msg')
+    var foto = window.document.getElementById ('imagem') 
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `agora são ${hora} horas.`
+  if ( hora >= 0 && hora < 13 ){
+
+    foto.src = 'diaft.jpg';
+    foto.alt = 'bom dia!'
+
+  }
+  else if (hora >= 13 && hora < 19 ){
+
+    foto.src = 'tardeft.jpg'
+    foto.alt = 'boa tarde' 
+  }
+  else {
+    foto.src = 'noiteft.jpg'
+    foto.alt = 'boa noite'
+  }
+}
